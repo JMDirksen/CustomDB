a5afc764c61994bb9c45d96e0d231942ddc91632
Jefta Dirksen <jeftadirksen@gmail.com>
2019-12-01 12:37:06 +0100

    Save

--- edit.php
+++ edit.php
@@ -1,9 +1,25 @@
 <?php
 require("functions.php");
 
+// DB Connect
+$mysqli = connect();
+
 // Save
-if(isset($_POST['submit'])) {
+if(isset($_POST['form_submit'])) {
   echo "Saving...";
+  
+  $setfields = [];
+  foreach($_POST as $key=>$value) {
+    if($key == "form_table") $table = $value;
+    if($key == "form_id") $id = $value;
+    if(substr($key,0,5) != "form_") $setfields[] = "$key = '$value'";
+  }
+  
+  // Query
+  $query = "update `$table` set ".join(", ",$setfields)." where id = $id";
+  $result = $mysqli->query($query);
+  if(!$result) die("Query error");
+  
   redirect("view.php?".$_SERVER['QUERY_STRING']);
 }
 
@@ -13,7 +29,6 @@ if(isset($_POST['submit'])) {
 <body>
 <a href="javascript:history.back()">Back</a>
 <form method="POST">
-<table>
 <?php
 
 // Input
@@ -26,24 +41,24 @@ if(!empty($_GET['id'])) {
 }
 else die("Missing/Wrong id variable");
 
-// DB Connect
-$mysqli = connect();
-
 // Query
 $result = $mysqli->query("select * from `$table` where id = $id");
 if(!$result) die("Query error");
 
-// Form
+// Table
+echo "<input type=\"hidden\" name=\"form_table\" value=\"$table\">";
+echo "<input type=\"hidden\" name=\"form_id\" value=\"$id\">";
+echo "<table>";
 while($row = $result->fetch_assoc()) {
   foreach($row as $key=>$value) {
     echo "<tr>";
     echo "<td>$key</td>";
-    echo "<td><input type=\"text\" value=\"$value\"></td>";
+    echo "<td><input type=\"text\" name=\"$key\" value=\"$value\"></td>";
     echo "</tr>\n";
   }
 }
 ?>
-<tr><td></td><td><input type="submit" name="submit" value="Save"</td></tr>
+<tr><td></td><td><input type="submit" name="form_submit" value="Save"</td></tr>
 </table>
 </form>
 </body>
