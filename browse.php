920a61a8b4fe888e5af495679a42cc5cbebe85f2
Jefta Dirksen <jeftadirksen@gmail.com>
2020-06-13 13:26:55 +0200

    refactor

--- browse.php
+++ browse.php
@@ -12,28 +12,25 @@ else die("Missing/Wrong table variable");
 button("<", "/");
 button("New", "edit.php?table=$table&new");
 
-// Table
+// Headers
+if(!$result = $mysqli->query("select * from `$table`")) die($mysqli->error);
 echo "<table>\n";
-
-// Fields
-if(!$result = $mysqli->query("show full columns from `$table`")) die($mysqli->error);
 echo "<tr>";
-while($field = $result->fetch_assoc()) {;
-  $name = $field['Field'];
-  $comment = $field['Comment'];
+$fields = $result->fetch_fields();
+foreach($fields as $field) {
+  $comment = getFieldComment($table, $field->name);;
   if(substr($comment,0,1)=="_") continue;
-  $display = $comment ?: $name;
+  $display = $comment ?: $field->name;
   echo "<th>$display</th>";
 }
 echo "</tr>\n";
 
 // Rows
-if(!$result = $mysqli->query("select * from `$table`")) die($mysqli->error);
 while($row = $result->fetch_assoc()) {
   $id = $row['id'];
   echo "<tr>";
-  foreach($row as $key=>$value) {
-    $comment = getFieldComment($table, $key);
+  foreach($row as $field=>$value) {
+    $comment = getFieldComment($table, $field);
     if(substr($comment,0,1)=="_") continue;
     echo "<td><a href=\"view.php?table=$table&id=$id\">$value</a></td>";
   }
