4b7bb145b0ebf0f0c3f0dd0e5649d8cf5701d58b
Jefta Dirksen <jeftadirksen@gmail.com>
2020-06-13 12:49:58 +0200

    Home / Index page

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+require("init.php");
+require("header.php");
+
+echo "<h1>".TITLE."</h1>";
+
+// List tables
+$q = "SELECT TABLE_NAME, TABLE_COMMENT FROM INFORMATION_SCHEMA.TABLES WHERE table_schema='".DB_NAME."'";
+if(!$result = $mysqli->query($q)) die($mysqli->error);
+while($row = $result->fetch_assoc()) {
+    $table = $row['TABLE_NAME'];
+    $comment = $row['TABLE_COMMENT'];
+    if(substr($comment,0,1)=="_") continue;
+    $display = $comment ?: $table;
+    echo "<a href=\"browse.php?table=$table\">$display</a><br/>";
+}
+
+require("footer.php");
