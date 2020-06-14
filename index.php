5053700d1ff397926ac8ac9ec152f39dc218418e
Jefta Dirksen <jeftadirksen@gmail.com>
2020-06-14 16:31:27 +0200

    Refactor

--- index.php
+++ index.php
@@ -3,13 +3,12 @@ require("init.php");
 require("header.php");
 
 // List tables
-$q = "SELECT TABLE_NAME, TABLE_COMMENT FROM INFORMATION_SCHEMA.TABLES WHERE table_schema='".DB_NAME."'";
+$q = "SELECT table_name FROM information_schema.tables WHERE table_schema = SCHEMA() ORDER BY table_name";
 if(!$result = $mysqli->query($q)) die($mysqli->error);
 while($row = $result->fetch_assoc()) {
-    $table = $row['TABLE_NAME'];
-    $td = getTableData($table);
+    $td = getTableData($row['table_name']);
     if($td['hide']) continue;
-    echo "<a href=\"browse.php?table=$table\">$td[caption]</a><br/>";
+    echo "<a href=\"browse.php?table=$row[table_name]\">$td[caption]</a><br/>";
 }
 
 require("footer.php");
